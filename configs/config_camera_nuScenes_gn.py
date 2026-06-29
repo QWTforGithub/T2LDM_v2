9a768df8efebf681b8237200d71db1bbd3843697
QWTforGithub <83633231+QWTforGithub@users.noreply.github.com>
2026-06-29 21:11:01 +0800

    Update config_camera_nuScenes_gn.py

--- configs/config_camera_nuScenes_gn.py
+++ configs/config_camera_nuScenes_gn.py
@@ -139,7 +139,7 @@ class TrainingConfig:
     dynamo_backend: str = None # "inductor", "no", None
     filer_keys =  [".n_",".temb.",".g_",] # ["n_","temb","g_",] # None
     checkpoint_dir: str = None
-    pretrained_checkpoint_dir: str = "/root/models/IJCV/nuScenes/partial_uncondiitional/diffusion_0000400000.pth"
+    pretrained_checkpoint_dir: str = "/root/models/IJCV/nuScenes/partial_uncondiitional_nuScenes/diffusion_0000400000.pth"
     # ---- training setting ----
 
     # ---- diffusion setting ----
