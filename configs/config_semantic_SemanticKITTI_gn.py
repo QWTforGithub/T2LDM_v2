0df4f75db62e4181d3e789ae8ea29c632ba9770e
QWTforGithub <83633231+QWTforGithub@users.noreply.github.com>
2026-06-29 21:03:21 +0800

    Update config_semantic_SemanticKITTI_gn.py

--- configs/config_semantic_SemanticKITTI_gn.py
+++ configs/config_semantic_SemanticKITTI_gn.py
@@ -139,7 +139,7 @@ class TrainingConfig:
     dynamo_backend: str = None # "inductor", "no", None
     filer_keys =  [".n_",".temb.",".g_",] # ["n_","temb","g_",] # None
     checkpoint_dir: str = None
-    pretrained_checkpoint_dir: str = "/root/models/IJCV/SemanticKITTI/unconditional/diffusion_0001700000.pth"
+    pretrained_checkpoint_dir: str = "/root/models/IJCV/SemanticKITTI/unconditional/diffusion_0000400000.pth"
     # ---- training setting ----
 
     # ---- diffusion setting ----
