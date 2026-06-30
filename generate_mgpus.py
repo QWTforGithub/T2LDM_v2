7e677f96064e5349f741030ea3b1810e06feb820
QWTforGithub <83633231+QWTforGithub@users.noreply.github.com>
2026-06-30 15:32:32 +0800

    Update generate_mgpus.py

--- generate_mgpus.py
+++ generate_mgpus.py
@@ -305,7 +305,7 @@ if __name__ == "__main__":
     root_path = "/ihoment/youjie10/qwt/model/T2LDM-plus"
     task = "BEV_nuScenes_gn"
     log = "20260515T001134"
-    name = "diffusion_0000980000.pth"
+    name = "diffusion_0000400000.pth"
     ckpt = f"{root_path}/logs/diffusion/{task}/{log}/models/{name}"
 
     # ckpt = "/root/models/checkpoints/nuScenes_semantic_0000400000.pth"
