407dfeabd9c469edcb79b1a24889c3b4d1651b1d
QWTforGithub <83633231+QWTforGithub@users.noreply.github.com>
2026-06-30 10:57:20 +0800

    Update config_sparsetodense_nuScenes_gn.py

--- configs/config_sparsetodense_nuScenes_gn.py
+++ configs/config_sparsetodense_nuScenes_gn.py
@@ -168,7 +168,7 @@ class TrainingConfig:
     dynamo_backend: str = None # "inductor", "no", None
     filer_keys =  [".n_",".temb.",".g_",] # ["n_","temb","g_",] # None
     checkpoint_dir: str = None
-    pretrained_checkpoint_dir: str = "/root/models/IJCV/nuScenes/unconditional/diffusion_0000610000.pth"
+    pretrained_checkpoint_dir: str = "/root/models/IJCV/nuScenes/unconditional/diffusion_0000400000.pth"
     # ---- training setting ----
 
     # ---- diffusion setting ----
